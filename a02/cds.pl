d27325a3de89d86e5c6ecf1895e4f8bb90add546
Alfred Chan <alfred8844@gmail.com>
2014-06-27 14:47:04 -0400

    added tor download prevention

--- a02/cds.pl
+++ a02/cds.pl
@@ -6,62 +6,11 @@ use diagnostics;
 # Auto flush stdout at each print
 $| = 1;
 
-my @ban_ip = (
-	# hackforums.net
-	"141.101.121.10",
-	"141.101.121.11",
-	"141.101.121.12",
-	"141.101.121.13",
-	"141.101.121.9",
-	# Makezine.com
-	"192.0.80.250",
-	"192.0.81.250",
-	"66.155.11.238",
-	"66.155.9.238",
-	"76.74.254.120",
-	"76.74.254.123",
-	# Hackaday.com
-	"192.0.82.250",
-	"192.0.83.250",
-	"66.155.11.244",
-	"66.155.9.244",
-	"76.74.255.117",
-	"76.74.255.123",
-	# Vcdquality.com
-	"208.93.110.120",
-	# Securityfocus.com
-	"143.127.139.110",
-	# hakin9.com
-	"148.251.235.73",
-	# Blackhat.com
-	"141.101.123.223",
-	"190.93.240.223",
-	# Sectools.org
-	"173.255.243.189",
-	# Hackedgadgets.com
-	"74.208.123.111",
-	# torproject.org
-	"38.229.72.16",
-	"82.195.75.101",
-	"86.59.30.40",
-	"93.95.227.222",
-	"38.229.72.14",
-	# gmail.com
-   "74.125.226.118",
-   "74.125.226.117",
-	# p.r.im
-  "54.243.190.39",
-	# google
-	"216[.]239[.]32[.]\\d+",
-	"64[.]244[.]160[.]\\d+",
-	"66[.]249[.]80[.]\\d+",
-	"72[.]14[.]192[.]\\d+",
-	"209[.]85[.]128[.]\\d+",
-	"65[.]102[.]\\d+[.]\\d+",
-	"74[.]125[.]\\d+[.]\\d+",
-	"64[.]18[.]\\d+[.]\\d+",
-	"207[.]126[.]144[.]\\d+",
-	"173[.]194[.]\\d+[.]\\d+",
+my %ban_ip = (
+	#Vcdquality.com
+	"208.93.110.120" => "Vcdquality.com",
+	#Securityfocus.com
+	"143.127.139.110" => "Securityfocus.com",
 );
 
 # ======== global variables ========
@@ -88,7 +37,7 @@ sub checkDNSLookup {
 	my $buffer;
 	foreach my $domain(@domains) {
 		if ($second =~ /(\d+.\d+.\d+.\d+).(\d+) >.*q: A\? $domain./) {
-			$buffer = "[Censored domain name lookup]: domain:$domain, src:$1:$2, host:";
+			$buffer = "[Censored domain name lookup]: domain:$domain, src:$1:$2, IP(s):";
 
 			# get rid of anything nameserver-related
 			$second =~ s/ns: .*//;
@@ -117,16 +66,21 @@ sub checkDNSLookup {
 sub checkIP {
 	my ($second, $buffer) = @_;
 
-	for my $ip (@ban_ip) {
-		if ($second =~ /(\d+.\d+.\d+.\d+).(\d+) > ($ip)/) {
-			my $clientIP = $1;
-			my $clientPort = $2;
-			my $banIP = $3;
-			for my $domain (@domains) {
-				if ($buffer =~ /($domain)/) {
-					print "[Censored domain connection attempt]: src:$clientIP:$clientPort, host:$banIP, domain:$1\n";
-					return 0;
-				}
+	if ($second =~ /(\d+.\d+.\d+.\d+).(\d+) > (\d+.\d+.\d+.\d+)/) {
+		my $clientIP = $1;
+		my $clientPort = $2;
+		my $banIP = $3;
+		for my $domain (@domains) {
+			if ($buffer =~ /[Hh][Oo][Ss][Tt]: $domain/) {
+				print "[Censored domain connection attempt]: src:$clientIP:$clientPort, host:$banIP, domain:$domain\n";
+				return 0;
+			}
+		}
+
+		for my $ip (keys %ban_ip) {
+			if ($buffer =~ /[Hh][Oo][Ss][Tt]: $ip/) {
+				print "[Censored domain connection attempt]: src:$clientIP:$clientPort, host:$banIP, domain:" . $ban_ip{$ip} . "\n";
+				return 0;
 			}
 		}
 	}
@@ -137,7 +91,7 @@ sub censoredURL {
 	my $payload = shift;
 	my $buffer;
 
-	if ($payload =~ /GET[.]((\/*[-_\w]+)+)[.]/) {
+	if ($payload =~ /(?:GET|POST|PUT|DELETE|HEAD) ((\/*[-_.\w\d]*)+)\s/) {
 		my $request = $1;
 		my @found_words = ();
 
@@ -152,7 +106,7 @@ sub censoredURL {
 			return 1;
 		}
 
-		if ($payload =~ /Host:[.](([.]*\w+)+)[.]/) {
+		if ($payload =~ /[hH][oO][sS][tT]: (([.]*\w+)+)/) {
 			my $domain = $1;
 			$buffer .= "[Censored Keyword - URL]: URL:$domain$request, keyword(s):";
 			$buffer .= join ", ", @found_words;
@@ -168,9 +122,15 @@ sub censoredPayload {
 	my $buffer;
 	my @found_words;
 
+	if ($second =~ /\d+.\d+.\d+.\d+.\d+ > \d+.\d+.\d+.\d+.(\d+).*A\?/) {
+		#if ($1 == 53) {
+			return 1;
+		#}
+	}
+
 	for my $keyword (@keywords) {
 		# match once
-		if ($payload =~ /[^a-zA-Z0-9]$keyword[^a-zA-Z0-9]/) {
+		if ($payload =~ /[^a-zA-Z0-9-]$keyword[^a-zA-Z0-9-]/) {
 			push @found_words, $keyword;
 		}
 	}
@@ -188,13 +148,40 @@ sub censoredPayload {
 	print "$buffer\n";
 }
 
+sub blockTorDownload {
+	my ($second, $payload) = @_;
+	my $buffer;
+	my $method;
+	my $domain;
+	my $request;
+
+	# avoid searching in payload if the packet is a DNS lookup
+	if ($second =~ /\d+.\d+.\d+.\d+.\d+ > \d+.\d+.\d+.\d+.(\d+).*A\?/) {
+		return 1;
+	} elsif ($payload =~ /GET ((\/*[-_.\w\d]*)*tor[._-]*browser[\w.-]*(?:exe|dmg|tar[.]xz))/) {
+		$request = $1;
+		$method = "HTTP";
+		if ($payload =~ /[hH][oO][sS][tT]: (([.]*\w+)+)/) {
+			$domain = $1;
+		} else {
+			return 1;
+		}
+	} else {
+		return 1;
+	}
+
+	print "[Tor download attempt]: Method:$method, Address:$domain$request\n";
+	return 0;
+}
+
 sub runAll {
 	my ($first, $second, $payload) = @_;
 	# only one alert per packet
 	(checkDNSLookup $second)
 		&& (checkIP $second, $payload)
 		&& (censoredURL $payload)
-		&& (censoredPayload $second, $payload);
+		&& (censoredPayload $second, $payload)
+		&& (blockTorDownload $second, $payload);
 }
 
 # ======== ``main" runner ========
@@ -211,14 +198,21 @@ while (my $line = <>) {
 	if ($line =~ /^[ ]/) {
 		$second = $line;
 	} elsif ($line =~ /^\t/) {
-		# push @buffer, substr $line, 51;
-		$payload .= substr $line, 51;
-	} else {
-		if (defined $first) {
-			runAll $first, $second, $payload;
+		my $temp = substr $line, 10, 39;
+		while ($temp =~ /([a-zA-Z0-9][a-zA-Z0-9])/g) {
+			$payload .= chr(hex($1));
 		}
-		$payload = "";
+	} else {
+		if (defined $first && defined $second) {
+			runAll $first, $second, $payload;
+#print "BEGIN======\n$payload\n";
+		}
 		$first = $line;
+		undef $second;
+		undef $payload;
 	}
 }
-runAll $first, $second, $payload;
+if (defined $first && defined $second) {
+	runAll $first, $second, $payload;
+#print "BEGIN======\n$payload\n";
+}
